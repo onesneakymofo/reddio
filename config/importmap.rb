5c1807aee7f4d95ba90ef52859802a0263e1dad7
Chris Autwell <cautwell@chriss-air.lan>
2023-01-02 10:21:31 -0600

    Add react

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,9 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/process-production.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/components", under: "components"
